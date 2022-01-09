c5f202646faf6c9aa15be126ba09ad70781a80e3
Umut Karabayraktar <umutka@gmail.com>
2022-01-10 00:02:48 +0300

    travis test 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there!! U</p>
+        <p>Hi there!! Umut</p>
         <a
           className="App-link"
           href="https://reactjs.org"
